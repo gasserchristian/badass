2bf0232b51222136c04c56e11dde0e268ed43f41
mchevron <max.chevron@gmail.com>
2017-12-07 14:17:52 +0100

    On branch master

--- sources/processus/Server.cpp
+++ sources/processus/Server.cpp
@@ -19,6 +19,7 @@ void Server::update(int tic) {
 		gnu_ << setprecision(3) << setw(15) << temp_data_[i]; //voir exo balle rebondissante pour alignement
 	}
 	gnu_ << endl;
+	temp_data_.clear();
 
 	if(tic == nTicks_-1) { //MAGIQUE NUMBER?
 		//Fermeture des fichiers:
@@ -43,8 +44,8 @@ void Server::log_journal(string text) {
 void Server::gnu_header(int zone_count) {
 	gnu_ << "# time[" << tick_unit_ << "]";
 	for(int i = 0; i < zone_count; i++) {
-		gnu_ << "    val_phen" << i+1 << "    state_curr"
-			 << i+1 << "   val_control" << i+1;
+		gnu_ << setw(15) << "val_phen" << i+1 << setw(15) << "state_curr"
+			 << i+1 << setw(15) << "val_control" << i+1;
 	}
 	gnu_ << endl;
 }
